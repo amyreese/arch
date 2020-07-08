f2a5631765b55eafb34ebcbf4d32d4963947c19e
John Reese <john@noswap.com>
2020-07-08 03:57:56 +0000

    AUR updates

--- plex-media-server-plexpass/plexmediaserver.conf.d
+++ plex-media-server-plexpass/plexmediaserver.conf.d
@@ -1,5 +1,9 @@
-LD_LIBRARY_PATH=/opt/plexmediaserver
-PLEX_MEDIA_SERVER_HOME=/opt/plexmediaserver
+# If your LC_ALL and LANG aren't en_US.UTF-8, plex crashes.
+# LC_ALL=en_US.UTF-8
+# LANG=en_US.UTF-8
+
+LD_LIBRARY_PATH=/usr/lib/plexmediaserver/lib
+PLEX_MEDIA_SERVER_HOME=/usr/lib/plexmediaserver
 PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plex
 PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
 PLEX_MEDIA_SERVER_TMPDIR=/tmp
